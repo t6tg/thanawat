921927c3cef06015fdaeb73f3b408865e323bc9a
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-23 20:50:36 +0700

    Dev Mode

--- src/components/DevMode.vue
+++ src/components/DevMode.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shadow-lg p-2 m-4 rounded">
+  <div class="shadow-lg p-2 m-4 rounded border-yellow-600 border-l-4 text-left">
     This site is an under development.
   </div>
 </template>
