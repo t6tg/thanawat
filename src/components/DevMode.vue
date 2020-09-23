e5ab0b6262be6b1ebc1cd4dff176f61c33846bda
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-23 20:55:56 +0700

    gramma

--- src/components/DevMode.vue
+++ src/components/DevMode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shadow-lg p-2 m-4 rounded border-yellow-600 border-l-4 text-left">
-    This site is an under development.
+    This site is under development.
   </div>
 </template>
 
