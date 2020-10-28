697f55edfc5847d8022c1685ab79bd65e5720d98
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-29 02:15:16 +0700

    Facebook Meta Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,28 @@ class MyDocument extends Document {
                         Let me be the one to love you more"
                     />
                     <link rel="icon" href="/favicon.png" />
+                    <meta
+                        property="og:url"
+                        content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+                    />
+                    <meta property="og:type" content="article" />
+                    <meta
+                        property="og:title"
+                        content="T6TG | Thanawat Gulati"
+                    />
+                    <meta
+                        property="og:description"
+                        content="I'll be waiting for you
+                        Here inside my heart
+                        I'm the one who wants to love you more
+                        You will see I can give you
+                        Everything you need
+                        Let me be the one to love you more"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://t6tg.com/toloveyoumore.png"
+                    />
                 </Head>
                 <body>
                     <Main />
