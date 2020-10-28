5c228518fe4ed8b2bdb8d3d96616bd723192ed1c
thanawatgulati <thanawatgulatijames@gmail.com>
2020-10-28 10:56:44 +0700

    Reset Project

--- src/components/DevMode.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="shadow-lg p-2 m-4 rounded border-yellow-600 border-l-4 text-left">
-    This site is under development.
-  </div>
-</template>
-
-<script>
-export default {};
-</script>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import './assets/tailwind.css'
-
-createApp(App).use(router).use(store).mount('#app')
