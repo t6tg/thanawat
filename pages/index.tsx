6c22c2c8f718898452f8e21c6f69b8f6600d575c
Thanawat Gulati <work.t6tg@gmail.com>
2021-01-01 20:02:14 +0700

    add ads.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,55 @@ import React, { ReactElement } from 'react'
 
 export default function index(): ReactElement {
     return (
-        <div className="flex flex-col h-screen items-center justify-center text-white">
-            <h2 className="sm:text-lg  font-hairline xl:text-6xl text-left md:text-4xl">
-                I'll be waiting for you <br />
-                Here inside my heart <br />
-                I'm the one who wants to love you more
-                <br />
-                You will see I can give you
-                <br />
-                Everything you need
-                <br />
-                Let me be the one to love you more 💔
-                <br />
-            </h2>
+        <div>
+            <div className="flex flex-col h-screen items-center justify-center text-white">
+                <h2 className="sm:text-lg  font-hairline xl:text-6xl text-left md:text-4xl">
+                    I'll be waiting for you <br />
+                    Here inside my heart <br />
+                    I'm the one who wants to love you more
+                    <br />
+                    You will see I can give you
+                    <br />
+                    Everything you need
+                    <br />
+                    Let me be the one to love you more 💔
+                    <br />
+                </h2>
+                <div className="grid grid-cols-2 gap-2">
+                    <a
+                        href="https://www.pathosting.co.th/?refer_id=115769"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="P&T Hosting Co., Ltd. :: บริการ | จดโดเมนเนม | เว็บโฮสติ้ง | VPS | Co-location |"
+                    >
+                        <img
+                            src="https://www.pathosting.co.th/affiliate/show_file?client=115769&slot=15"
+                            alt="P&T Hosting Co., Ltd. :: บริการ | จดโดเมนเนม | เว็บโฮสติ้ง | VPS | Co-location |"
+                            style={{
+                                border: '0px',
+                                width: '468px',
+                                height: '60px'
+                            }}
+                        />
+                    </a>
+                    <a
+                        href="https://www.pathosting.co.th/?refer_id=115769"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="P&T Hosting Co., Ltd. :: บริการ | จดโดเมนเนม | เว็บโฮสติ้ง | VPS | Co-location |"
+                    >
+                        <img
+                            src="https://www.pathosting.co.th/affiliate/show_file?client=115769&slot=9"
+                            alt="P&T Hosting Co., Ltd. :: บริการ | จดโดเมนเนม | เว็บโฮสติ้ง | VPS | Co-location |"
+                            style={{
+                                border: '0px',
+                                width: '468px',
+                                height: '60px'
+                            }}
+                        />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
