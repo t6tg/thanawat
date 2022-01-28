13344e3217da9fb0879fcfef32e29de7c00b31bf
Thanawat Gulati <work.t6tg@gmail.com>
2022-01-28 23:15:12 +0700

    feat: add css feed and update dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,30 @@ const Index = ({ data, spotify_data }) => {
     <div>
       <Head>
         <title>T6TG | Thanawat Gulati</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="description" content="Thanawat Gulati • CSKMUNTB#32 | t6tg.com | Google Developer Student Clubs Lead | Microsoft Learn Student Ambassador (MLSA)" />
-        <meta name="og:description" content="Thanawat Gulati • CSKMUNTB#32 | t6tg.com | Google Developer Student Clubs Lead | Microsoft Learn Student Ambassador (MLSA)"/>
-        <meta name="keywords" content="t6tg, T6TG, james, Thanawat, thanawat, gulati, Gulati, m.mjames, mjames" />
-        <meta name="author" content="Thanawat Gulati"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Thanawat Gulati • CSKMUNTB#32 | t6tg.com | Google Developer Student Clubs Lead | Microsoft Learn Student Ambassador (MLSA)"
+        />
+        <meta
+          name="og:description"
+          content="Thanawat Gulati • CSKMUNTB#32 | t6tg.com | Google Developer Student Clubs Lead | Microsoft Learn Student Ambassador (MLSA)"
+        />
+        <meta
+          name="keywords"
+          content="t6tg, T6TG, james, Thanawat, thanawat, gulati, Gulati, m.mjames, mjames"
+        />
+        <meta name="author" content="Thanawat Gulati" />
         <meta name="robots" content="index,follow" />
         <meta property="og:title" content="T6TG • Thanawat Gulati" />
         <meta property="og:site_name" content="T6TG" />
         <meta property="og:image" content="https://to.t6tg.com/preview_img" />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed for t6tg.com"
+          href="/rss.xml"
+        />
       </Head>
       <Header nowPlay={spotify_data} />
       <Blog data={data} />
